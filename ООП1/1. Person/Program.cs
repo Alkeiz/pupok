beddbfb7f35e8fbcd201ce5d369815b81686ea09
Alkeiz <lizas1447@gmail.com>
2025-10-19 20:34:05 +0300

    Add files via upload

---
+++ ООП1/1. Person/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace um
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Мафусаил";
+            person.Age = 69;
+            person.SayHello(person.Name, person.Age);
+            Console.ReadKey();
+        }
+    }
+}
