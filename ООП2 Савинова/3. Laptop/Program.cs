95d5b17db4ef0f346ff2fc323110494ea9c7ecea
Alkeiz <lizas1447@gmail.com>
2025-10-26 19:50:19 +0300

    Add files via upload

---
+++ ООП2 Савинова/3. Laptop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using яфв1;
+
+namespace zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            point input = new point(3.5);
+            input.Show();
+        }
+    }
+}
+
---
+++ ООП2 Савинова/4. Point/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(3.5, 7.2);
+            Point point2 = new Point(5);
+            Point point3 = new Point();
+            point1.Show();
+            point2.Show();
+            point3.Show();
+        }
+    }
+}
